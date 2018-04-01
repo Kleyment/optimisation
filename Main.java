e94faaf7722d43458f90aa96957a6a632a7b4104
Thouvenin <thouveninvalentin@gmail.com>
2018-04-01 14:37:31 +0200

    Ajout constructeur de copie

--- Main.java
+++ Main.java
@@ -3,10 +3,12 @@ package optimisation;
 public class Main {
 
 	public static void main(String[] args) {
-		Configuration conf=new Configuration(8, 5,4,5,3,6,7);
+		Configuration conf=new Configuration(2, 5,4,5,3,6,7);
 		Solution s=new Solution(conf,true);
 		System.out.println(s);
 		System.out.println(s.evaluer());
+		Solution s2 = new Solution(s);
+		System.out.println("s2 : "+ s2);
 		int[] tab = s.creerTableau();
 		for (int i = 0; i < tab.length; i++) {
 			System.out.print(tab[i]);
@@ -15,6 +17,7 @@ public class Main {
 		algoGen.mutation(s);
 		System.out.println(s);
 		System.out.println(s.evaluer());
+		System.out.println("s2 :"+ s2);
 	}
 
 }
