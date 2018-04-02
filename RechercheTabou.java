4767a41a2c635b3e5046020f01722e927b72d4ec
Thouvenin <thouveninvalentin@gmail.com>
2018-04-02 16:03:51 +0200

    Recherche tabou

--- RechercheTabou.java
+++ RechercheTabou.java
@@ -7,28 +7,52 @@ public class RechercheTabou {
 	private ArrayList<Solution> listeTabou;
 	private Configuration conf;
 	private Solution solution;
+	private int tailleListe;
 	
 	public RechercheTabou(Configuration conf, int tailleListe) {
 		this.conf = conf;
-		listeTabou = new ArrayList<Solution>(tailleListe);
+		listeTabou = new ArrayList<Solution>();
 		solution = new Solution(conf, true);
-		
+		this.tailleListe = tailleListe;
 	}
 	
 	public void mouvement() {
 		boolean equals = false;
-		while (!listeTabou.isEmpty()) {
-			Solution solution_actuelle=new Solution(this.solution);
-			AlgoGenetique.mutation(solution_actuelle);
+		ArrayList<Solution> listeVoisins = new ArrayList<Solution>();
+		Solution solutionActuelle = solution;
+		int eval;
+		while (listeTabou.size() < tailleListe) {
+			Solution voisin1=new Solution(this.solution);
+			AlgoGenetique.mutation(voisin1);
+			listeVoisins.add(voisin1);
+			Solution voisin2=new Solution(this.solution);
+			AlgoGenetique.mutation(voisin2);
+			listeVoisins.add(voisin2);
+			Solution voisin3=new Solution(this.solution);
+			AlgoGenetique.mutation(voisin3);
+			listeVoisins.add(voisin3);
+			Solution voisin4=new Solution(this.solution);
+			AlgoGenetique.mutation(voisin4);
+			listeVoisins.add(voisin4);
+			eval = voisin1.getEvaluation();
+			solutionActuelle = voisin1;
+			for (Solution v : listeVoisins) {
+				if (v.getEvaluation() > eval) {
+					eval = v.getEvaluation();
+					solutionActuelle = v;
+				}
+			}
+			listeVoisins.clear();
 			for (Solution s : listeTabou) {
-				if (solution_actuelle.equals(s)) {
+				if (solutionActuelle.equals(s)) {
 					equals = true;
 				}
 			}
 			if (!equals) {
-				listeTabou.add(solution_actuelle);
-				solution = solution_actuelle;
+				listeTabou.add(solutionActuelle);
+				solution = solutionActuelle;
 			}
+			equals = false;
 		}
 	}
 	
